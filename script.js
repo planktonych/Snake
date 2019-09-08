4fa2373689b60d91d22f03251aa015c20a79e70d
Richard Gobert <dev.richard.g@gmail.com>
2019-09-08 16:06:28 +0200

    creation of the snake and management of the snake direction

--- script.js
+++ script.js
@@ -5,31 +5,124 @@ window.onload = function () /*fonction d'affichage de la fenêtre de jeux à l'o
     let blockSize = 30
     let ctx;
     let delay = 100;
-    let xCoord = 0;
-    let yCoord = 0;
+    let snakee;
 
     init();
 
     function init()
     {
 
-        canvas = document.createElement('canvas'); /*fonction canvas pour faire apparaitre une zone interactive (déssin) sur son HTML.*/
+        let canvas = document.createElement('canvas'); /*fonction canvas pour faire apparaitre une zone interactive (déssin) sur son HTML.*/
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
-        canvas.style.border = "1px solid";
+        canvas.style.border = "2px solid";
         document.body.appendChild(canvas); /*Appelation du canvas dans le body*/
         ctx = canvas.getContext('2d');/* variable pour le contexte de notre canvas '2d'*/
-        refreshcanvas();
+        snakee = new Snake([[6,4], [5,4], [4,4]], "right");
+        refreshCanvas();
     }
 
-    function refreshcanvas()
+    function refreshCanvas()
     {
-        xCoord += 5;
-        yCoord += 5;
-        ctx.clearRect(0,0,canvasWidth,canvasHeight);
-        ctx.fillStyle = "#ff0000";
-        ctx.fillRect(xCoord, yCoord, 100, 50); /* les deux premières valeurs sont le positionnement horizontal et vertical.
+        ctx.clearRect(0,0,canvasWidth,canvasHeight);/* pour effacer une zone que l'on souhaite */
+        /*ctx.fillRect(xCoord, yCoord, 100, 50); les deux premières valeurs sont le positionnement horizontal et vertical.
             Les deux autres sont la dimension de notre dessin comme là un rectangle de 100 px sur 50 px.*/
-        setTimeout(refreshcanvas,delay);
+        snakee.advance();
+        snakee.draw();
+        setTimeout(refreshCanvas, delay);/* pour refresh une fonction après un délai passé */
+    }
+
+    function drawBlock(ctx, position)
+    {
+        let x = position[0] * blockSize;
+        let y = position[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);/* les deux premières valeurs sont le positionnement horizontal et vertical.
+        Les deux autres sont la dimension de notre dessin comme là un rectangle de 100 px sur 50 px.*/
+
+    }
+
+    function Snake(body, direction)
+    {
+        this.body = body;
+        this.direction = direction;
+        this.draw = function()
+        {
+            ctx.save();
+            ctx.fillStyle = "#ff0000";
+            for(let i = 0; i < this.body.length; i++)
+            {
+                drawBlock(ctx, this.body[i]);
+            }
+            ctx.restore();
+        };
+        this.advance= function()
+        {
+            let nextPosition = this.body[0].slice();
+            switch(this.direction)
+            {
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                case "up":
+                    nextPosition[1] -= 1;
+                    break;
+                default:
+                    throw("Invalid Direction");
+            }
+            this.body.unshift(nextPosition);
+            this.body.pop()
+        };
+        this.setDirection = function(newDirection)
+        {
+            let allowedDirection;
+            switch(this.direction)
+            {
+                case "left":
+                case "right":
+                    allowedDirection = ["up", "down"];
+                    break;
+                case "down":
+                case "up":
+                    allowedDirection = ["left", "right"];
+                    break;
+                default:
+                    throw("Invalid Direction");
+            }
+            if (allowedDirection.indexOf(newDirection) > -1)
+            {
+                this.direction = newDirection;
+            }
+        };
+    }
+
+    document.onkeydown = function handleKeyDown(e)
+    {
+        let key = e.keyCode;
+        let newDirection;
+        switch(key)
+        {
+            case 37:
+                 newDirection = "left";
+                break;
+            case 38:
+                newDirection = "up";
+                break;
+            case 39:
+                newDirection = "right";
+                break;
+            case 40:
+                newDirection = "down";
+                break;
+            default:
+                return;
+
+        }
+    snakee.setDirection(newDirection);
     }
 }
